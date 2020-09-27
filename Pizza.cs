ac36870a99d88e6e05ca5eb0dc2ce564d8a185f8
ETR <edcarroll34@gmail.com>
2020-09-27 19:19:39 -0400

    have 5 of 8 pizza menu options working properly.

---
+++ Pizza.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Choose_Your_Class
+{
+    public class Pizza
+    {
+        public Pizza()
+        {
+            Price = 20;
+        }
+        public int Price { get; set; }
+
+        public string PizzaSize()
+        {
+                string[] sizeOfPie = new string[3];
+            sizeOfPie[0] = "Large";
+            sizeOfPie[1] = "Larger";
+            sizeOfPie[2] = "Largest";
+            if (Console.ReadLine() == sizeOfPie[0])
+                {
+                Console.WriteLine($"You have ordered a {sizeOfPie[0]} pizza");
+            }
+            else if (Console.ReadLine() == sizeOfPie[1])
+            {
+                Console.WriteLine($"You have ordered a {sizeOfPie[1]} pizza!");
+            }
+            else
+            {
+                Console.WriteLine($"You ordered a {sizeOfPie[2]} pizza pie! Mamma mia!");
+            }
+        }
+        public int OrderDetails()
+        {
+            return Price;
+        }
+        public int IsDelivery()
+
+        { 
+           return 
+        }
+        public void AddPop()
+        {
+            Price += 2;
+        }
+        public void Toppings()
+        {
+            Price += 4;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,77 @@ namespace Choose_Your_Class
     {
         static void Main(string[] args)
         {
-            
+            Pizza pizza = new Pizza();
+
+            Console.WriteLine("Welcome to the pizza pie!");
+
+            bool makingOrder = true;
+            while (makingOrder)
+            {
+
+
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.WriteLine("1. Choose pizza size - they're all large!");
+                Console.WriteLine("2. Want the works? Add extra toppings for $4.");
+                Console.WriteLine("3. Is this for delivery?");
+                Console.WriteLine("4. When do you want your order ready?");
+                Console.WriteLine("5. Thirsty? Add a pop for $2!");
+                Console.WriteLine("6. Check order details.");
+                Console.WriteLine("7. Confirm your order here!");
+                Console.WriteLine("8. Exit and order burgers.");
+
+                var pizzaChoice = Console.ReadLine();
+
+                switch (pizzaChoice)
+                {
+
+                    case "1":
+                        Console.WriteLine("What size pizza would you like? Large, larger or largest?");
+                        string pizzaSize = Console.ReadLine();
+                        Console.WriteLine($"Ok, one {pizzaSize} pizza coming up!");
+                break;
+
+                    case "2":
+                        pizza.Toppings();
+                Console.WriteLine("I'll add pepperoni, extra cheese and pineapple to your pizza. That'll add $4 to your order.");
+                break;
+
+                    case "3":
+                        pizza.IsDelivery();
+                        break;
+
+                    case "4":
+
+                        break;
+
+                    case "5":
+                        pizza.AddPop();
+                Console.WriteLine("An ice cold soda is tasty with a hot pizza pie! That'll be an extra $2.");
+                break;
+
+                    case "6":
+                        int currentPrice = pizza.OrderDetails();
+                Console.WriteLine($"Your order will cost {currentPrice:C2}");
+                break;
+
+                    case "7":
+                        int finalPrice = pizza.OrderDetails();
+                        string finalPizzaSize = pizza.PizzaSize();
+                Console.WriteLine($"That will be {finalPrice:C2}, see you soon!");
+                makingOrder = false;
+                break;
+
+                    case "8":
+                        Console.WriteLine("I didn't feel like doing work anyway. Bye.");
+                makingOrder = false;
+                break;
+
+                default:
+                        Console.WriteLine("That doesn't compute, my friend! Pick another option!");
+                break; 
+                }
+            }
         }
-    }
-}
+                }
+            
+            }
